8ec005bec7884a2e0d4e8163aa77f2179715fb72
DESKTOP-HKKP34A\E5530 <new.developer297@gmail.com>
2020-11-30 18:35:18 +0200

    Коммит

--- ui_resultwindow.h
+++ ui_resultwindow.h
@@ -19,7 +19,6 @@
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
-#include <QtWidgets/QTextBrowser>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -34,12 +33,10 @@ public:
     QLabel *system_2;
     QLabel *system_3;
     QLabel *system_4;
-    QLabel *mark;
     QLabel *balls_1;
     QLabel *balls_2;
     QLabel *balls_3;
     QLabel *balls_4;
-    QTextBrowser *system_info;
     QPushButton *first;
     QPushButton *direction;
     QMenuBar *menubar;
@@ -73,9 +70,6 @@ public:
         system_4 = new QLabel(centralwidget);
         system_4->setObjectName(QStringLiteral("system_4"));
         system_4->setGeometry(QRect(70, 330, 331, 31));
-        mark = new QLabel(centralwidget);
-        mark->setObjectName(QStringLiteral("mark"));
-        mark->setGeometry(QRect(70, 390, 111, 16));
         balls_1 = new QLabel(centralwidget);
         balls_1->setObjectName(QStringLiteral("balls_1"));
         balls_1->setGeometry(QRect(430, 180, 91, 31));
@@ -98,9 +92,6 @@ public:
         balls_4->setGeometry(QRect(430, 330, 101, 31));
         balls_4->setStyleSheet(QLatin1String("font-size:14px;\n"
 "font-weight:700;"));
-        system_info = new QTextBrowser(centralwidget);
-        system_info->setObjectName(QStringLiteral("system_info"));
-        system_info->setGeometry(QRect(70, 420, 601, 121));
         first = new QPushButton(centralwidget);
         first->setObjectName(QStringLiteral("first"));
         first->setGeometry(QRect(70, 570, 111, 41));
@@ -152,16 +143,10 @@ public:
         system_2->setText(QApplication::translate("ResultWindow", "<html><head/><body><p><span style=\" font-size:14pt;\">\320\240\320\276\320\261\320\276\321\202\320\260 \320\267 \321\202\320\265\321\205\320\275\321\226\320\272\320\276\321\216</span></p></body></html>", Q_NULLPTR));
         system_3->setText(QApplication::translate("ResultWindow", "<html><head/><body><p><span style=\" font-size:14pt;\">\320\242\320\262\320\276\321\200\321\207\320\260 \321\200\320\276\320\261\320\276\321\202\320\260</span></p></body></html>", Q_NULLPTR));
         system_4->setText(QApplication::translate("ResultWindow", "<html><head/><body><p><span style=\" font-size:14pt;\">\320\240\320\276\320\261\320\276\321\202\320\260 \320\267\321\226 \320\267\320\275\320\260\320\272\320\276\320\262\320\276\321\216 \321\201\320\270\321\201\321\202\320\265\320\274\320\276\321\216</span></p></body></html>", Q_NULLPTR));
-        mark->setText(QApplication::translate("ResultWindow", "<html><head/><body><p><span style=\" font-size:12pt; font-weight:600;\">\320\237\321\200\320\270\320\274\321\226\321\202\320\272\320\260</span></p></body></html>", Q_NULLPTR));
         balls_1->setText(QApplication::translate("ResultWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#FFA500;\">10%</span></p></body></html>", Q_NULLPTR));
         balls_2->setText(QApplication::translate("ResultWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ffff00;\">50%</span></p></body></html>", Q_NULLPTR));
         balls_3->setText(QApplication::translate("ResultWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ffff00;\">60%</span></p></body></html>", Q_NULLPTR));
         balls_4->setText(QApplication::translate("ResultWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#55ff00;\">100%</span></p></body></html>", Q_NULLPTR));
-        system_info->setHtml(QApplication::translate("ResultWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">\302\253\320\233\321\216\320\264\320\270\320\275\320\260 - \321\205\321\203\320\264\320\276\320\266\320\275\321\226\320\271 \320\276\320\261\321\200\320\260\320\267\302\273 - \321\206\320\265 \320\262\321\201\321\226 \321\202\320\262\320\276\321\200\321\207\321\226 \321\201\320\277\320\265\321\206\321\226\320\260\320\273\321\214\320\275\320\276\321\201\321\202\321\226.</span></p></body></html>", Q_NULLPTR));
         first->setText(QApplication::translate("ResultWindow", "\320\222 \320\274\320\265\320\275\321\216", Q_NULLPTR));
         direction->setText(QApplication::translate("ResultWindow", "\320\236\320\261\321\200\320\260\321\202\320\270 \320\275\320\260\320\277\321\200\321\217\320\274\320\276\320\272 \320\277\321\200\320\276\321\204\320\265\321\201\321\226\321\227", Q_NULLPTR));
     } // retranslateUi
