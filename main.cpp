3fb89a0371a5ab1c71c757354662f895b88fc3fa
MichkaB0 <66625662+MichkaB0@users.noreply.github.com>
2020-12-05 22:09:28 +0200

    fix exception

--- main.cpp
+++ main.cpp
@@ -5,20 +5,27 @@
 #include "myexception.h"
 
 QWidget *pMainWidget = nullptr;
-
+//22
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
-    MyException::setDeveloperEMail("artem.palamarchyuk@gmail.com");
-    MyException::setDeveloperFIO("Артем Паламарчук");
-    MyException::setDeveloperTel("+38 096 455 57 48");
-    MyException::setErrorMessageTemplate("При работе программы возникла ошибка в методе {method}\nОшибка: {errorMsg}\n\nЗа детальной информацией обратитесь к разработчику. \n{developerEMail}\n{developerTel}\n{developerFIO}");
     try {
+
+        MyException::setDeveloperEMail("artem.palamarchyuk@gmail.com");
+        MyException::setDeveloperFIO("Артем Паламарчук");
+        MyException::setDeveloperTel("+38 096 455 57 48");
+        MyException::setErrorMessageTemplate("При работе программы возникла ошибка в методе {method}\nОшибка: {errorMsg}\n\nЗа детальной информацией обратитесь к разработчику. \n{developerEMail}\n{developerTel}\n{developerFIO}");
+
+
         MainWindow *pMainWindows = new MainWindow;
         pMainWidget = pMainWindows;
         pMainWindows->setWindowTitle("Застосунок");
         pMainWindows->setWindowFlags(Qt::Dialog);
         pMainWindows->show();
+
+        return a.exec();
+
     } catch (MyException &exception) {
         QMessageBox::critical(pMainWidget, exception.methodName(), exception.getErrorMessage());
         return -1;
@@ -26,5 +33,5 @@ int main(int argc, char *argv[])
         QMessageBox::critical(pMainWidget, "UNKNOWN_ERROR", "UNKNOWN_ERROR");
         return -1;
     }
-    return a.exec();
+
 }
